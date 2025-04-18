cba241ac52d2576e60935cbf6e31f9399d600675
ExtraSeven-code <apexexe124@gmail.com>
2025-04-18 15:27:43 -0500

    yes
    yes

---
+++ RV_aviones/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 1f;
+    public Vector3 direction;
+    private float lifetime = 2f;
+            
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+        lifetime -= Time.deltaTime;
+        if(lifetime < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ RV_aviones/Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 1f;
+    public Vector3 direction;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
